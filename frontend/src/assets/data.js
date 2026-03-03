ebd48892f7987572895a3ac0e8c80ddc5bb5cffc
SAmaksh5097 <samaksharzare.cs2004@gmail.com>
2026-03-03 17:35:04 +0530

    assests changed

--- frontend/src/assets/data.js
+++ frontend/src/assets/data.js
@@ -1,9 +1,12 @@
 // projects
+import propl from '../assets/proplanss.png'
+import dailyp from '../assets/dailyplss.png'
+import portf from '../assets/portfolioss.png'
 export const projects = [
     {
         title: 'ProPlanner',
         description: 'Full stack project roadmap generator built with React, Node.js, and Google Gemini API',
-        image: 'https://private-user-images.githubusercontent.com/167022519/556534466-1cb6da56-e60d-4bc4-8338-c1832005361a.png?jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NzI1MjYxMjgsIm5iZiI6MTc3MjUyNTgyOCwicGF0aCI6Ii8xNjcwMjI1MTkvNTU2NTM0NDY2LTFjYjZkYTU2LWU2MGQtNGJjNC04MzM4LWMxODMyMDA1MzYxYS5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjYwMzAzJTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI2MDMwM1QwODE3MDhaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT1lNzYyMzk0ZDAwMzFjZWUyNTc2MDQ2NWU4YTcxMDJhYTc4YTJhODM5ODM5NTQwYWFjN2FhNjYzZGMxZDg5NTkzJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.5fZkdGVEEYOr5QrVCezoPDe17mKXchpALOgqYeniyhk',
+        image: propl,
         link: 'https://pro-planner-app.vercel.app/',
         github:'https://github.com/SAmaksh5097/ProPlanner-App'
 
@@ -11,14 +14,14 @@ export const projects = [
     {
         title: 'Daily Planner',
         description: 'A simple daily planner app built with React and Tailwind CSS',
-        image: 'https://private-user-images.githubusercontent.com/167022519/534373214-126747c9-0ddb-4900-bc17-6e2de836361f.png?jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NzI1MjYwODIsIm5iZiI6MTc3MjUyNTc4MiwicGF0aCI6Ii8xNjcwMjI1MTkvNTM0MzczMjE0LTEyNjc0N2M5LTBkZGItNDkwMC1iYzE3LTZlMmRlODM2MzYxZi5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjYwMzAzJTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI2MDMwM1QwODE2MjJaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT1iMWJjNjdkYTE4Y2ZjNjY0YTg2OTJiYjNmNTk4MjkyNjgxNmY5ZjljZDVmZWE3MjA5MjI1MzA3NGVjNzc2OTU5JlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.UuU0qO11MHsoVQyKP6pBq7HumiQajbl0tMdWAAXBq7o',
-        link: 'https:daily-planner-app-black.vercel.app',
+        image: dailyp,
+        link: 'https://daily-planner-app-black.vercel.app',
         github:'https://github.com/SAmaksh5097/Daily-Planner-App'
     },
     {
         title: 'Personal Portfolio Website',
         description: 'A personal portfolio website built with React and Tailwind CSS',
-        image:'https://private-user-images.githubusercontent.com/167022519/557463295-6ecf0534-4259-4959-b9de-b33f68d0dc7b.png?jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NzI1Mzc3ODMsIm5iZiI6MTc3MjUzNzQ4MywicGF0aCI6Ii8xNjcwMjI1MTkvNTU3NDYzMjk1LTZlY2YwNTM0LTQyNTktNDk1OS1iOWRlLWIzM2Y2OGQwZGM3Yi5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjYwMzAzJTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI2MDMwM1QxMTMxMjNaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT1iNzhhOWYxNTBjNGQzNDJhYjZmMTZjMzZiOGJkZThkZWNkYjM5NTFkNWQ5Mzk4MDAzOThiMmZhODc2NDZiZDc2JlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.Jp3PumBBIC9K6gNyLCuKpedo1SnPt_WelXNxSGBN9PQ',
+        image: portf,
         github:'https://github.com/SAmaksh5097/Personal-Portfolio-Website'
     },
 ]
